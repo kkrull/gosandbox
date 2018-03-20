718f24a22b35938ac95fdcc7e8fa6e6debc23028
Kyle Krull <kkrull@8thlight.com>
2018-03-20 14:39:35 -0500

    Helper functions for Board#FindWinner

--- minimax/minimax_test.go
+++ minimax/minimax_test.go
@@ -7,12 +7,23 @@ import (
 	. "github.com/kkrull/gosandbox/minimax"
 )
 
+func nobodyClaimsVictory(string, Space) string { return "" }
+func automaticWin(id string) func(string, Space) string {
+	return func(ifPlayer string, claimsSpace Space) string {
+		if claimsSpace.Id() == id {
+			return ifPlayer
+		} else {
+			return ""
+		}
+	}
+}
+
 var _ = Describe("Board", func() {
 	Describe("#Minimax", func() {
 		Context("when there is only 1 space", func() {
 			It("picks that space", func() {
 				var spaces = []Space{OpenSpace("open")}
-				var board = NewBoard(spaces, func(string, Space) string { return "" })
+				var board = NewBoard(spaces, nobodyClaimsVictory)
 				Expect(board.Minimax("anybody")).To(Equal("open"))
 			})
 		})
@@ -20,7 +31,7 @@ var _ = Describe("Board", func() {
 		Context("when there is only 1 available space", func() {
 			It("picks that space", func() {
 				var spaces = []Space{ClosedSpace("closed"), OpenSpace("open")}
-				var board = NewBoard(spaces, func(string, Space) string { return "" })
+				var board = NewBoard(spaces, nobodyClaimsVictory)
 				Expect(board.Minimax("anybody")).To(Equal("open"))
 			})
 		})
@@ -28,14 +39,7 @@ var _ = Describe("Board", func() {
 		Context("when there are 2 or more available spaces with different outcomes", func() {
 			It("picks the space where the maximizing player wins, when it is that player's turn", func() {
 				var spaces = []Space{OpenSpace("worthless"), OpenSpace("victory")}
-				var board = NewBoard(spaces, func(player string, space Space) string {
-					if space.Id() == "victory" {
-						return player
-					} else {
-						return ""
-					}
-				})
-
+				var board = NewBoard(spaces, automaticWin("victory"))
 				Expect(board.Minimax("max")).To(Equal("victory"))
 			})
 		})
