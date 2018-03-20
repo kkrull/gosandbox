066641e85ed541ab7ea9b0dc662b623b7bf589dd
Kyle Krull <kkrull@8thlight.com>
2018-03-20 15:03:48 -0500

    Scoring game-winning moves for both players

--- minimax/minimax.go
+++ minimax/minimax.go
@@ -2,19 +2,30 @@ package minimax
 
 //import "fmt"
 
-func NewBoard(spaces []Space, findWinner func(string, Space) string) Board {
-	return Board{Spaces: spaces, FindWinner: findWinner}
+func NewBoard(maxPlayer string, minPlayer string, spaces []Space, findWinner func(string, Space) string) Board {
+	return Board{
+		maxPlayer:  maxPlayer,
+		minPlayer:  minPlayer,
+		FindWinner: findWinner,
+		Spaces:     spaces,
+	}
 }
 
 type Board struct {
-	Spaces     []Space
+	maxPlayer  string
+	minPlayer  string
 	FindWinner func(ifPlayer string, claimsSpace Space) string
+	Spaces     []Space
 }
 
 func (board Board) Minimax(player string) (string, int) {
 	for _, space := range board.Spaces {
 		if board.FindWinner(player, space) == player {
-			return space.Id(), 1
+			if player == board.maxPlayer {
+				return space.Id(), 1
+			} else {
+				return space.Id(), -1
+			}
 		}
 	}
 
--- minimax/minimax_test.go
+++ minimax/minimax_test.go
@@ -20,34 +20,34 @@ func automaticWin(id string) func(string, Space) string {
 
 var _ = Describe("Board", func() {
 	Describe("#Minimax", func() {
+		var (
+			board Board
+			space string
+			score int
+		)
+
 		Context("when there is only 1 space", func() {
 			It("picks that space", func() {
-				var spaces = []Space{OpenSpace("open")}
-				var board = NewBoard(spaces, nobodyClaimsVictory)
-				var space, _ = board.Minimax("anybody")
+				spaces := []Space{OpenSpace("open")}
+				board = NewBoard("max", "min", spaces, nobodyClaimsVictory)
+				space, _ = board.Minimax("anybody")
 				Expect(space).To(Equal("open"))
 			})
 		})
 
 		Context("when there is only 1 available space", func() {
 			It("picks that space", func() {
-				var spaces = []Space{ClosedSpace("closed"), OpenSpace("open")}
-				var board = NewBoard(spaces, nobodyClaimsVictory)
-				var space, _ = board.Minimax("anybody")
+				spaces := []Space{ClosedSpace("closed"), OpenSpace("open")}
+				board = NewBoard("max", "min", spaces, nobodyClaimsVictory)
+				space, _ = board.Minimax("anybody")
 				Expect(space).To(Equal("open"))
 			})
 		})
 
 		Context("when there are 2 or more available spaces with different outcomes", func() {
-			var (
-				board Board
-				space string
-				score int
-			)
-
 			BeforeEach(func() {
-				var spaces = []Space{OpenSpace("worthless"), OpenSpace("victory")}
-				board = NewBoard(spaces, automaticWin("victory"))
+				spaces := []Space{OpenSpace("worthless"), OpenSpace("victory")}
+				board = NewBoard("max", "min", spaces, automaticWin("victory"))
 			})
 
 			Context("when it is the maximizing player's turn", func() {
@@ -61,13 +61,16 @@ var _ = Describe("Board", func() {
 				})
 			})
 
-			//It("picks the space where the minimizing player wins, when it is that player's turn", func() {
-			//	var spaces = []Space{OpenSpace("worthless"), OpenSpace("victory")}
-			//	var board = NewBoard(spaces, automaticWin("victory"))
-			//	var space, score = board.Minimax("min")
-			//	Expect(space).To(Equal("victory"))
-			//	Expect(score).To(Equal(-1))
-			//})
+			Context("when it is the minimizing player's turn", func() {
+				It("picks the space where the maximizing player loses", func() {
+					space, score = board.Minimax("min")
+					Expect(space).To(Equal("victory"))
+				})
+				It("returns a negative score for that space", func() {
+					space, score = board.Minimax("min")
+					Expect(score).To(Equal(-1))
+				})
+			})
 		})
 	})
 })
