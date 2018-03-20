d3b243065a9610348551aec7149826f6c4af4268
Kyle Krull <kkrull@8thlight.com>
2018-03-20 11:19:03 -0500

    Empty minimax test
    I think I may have been having path issues with the other project

---
+++ minimax/minimax_test.go
@@ -0,0 +1,12 @@
+package minimax_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	//. "github.com/onsi/gomega"
+
+	//. "github.com/kkrull/gosandbox/minimax"
+)
+
+var _ = Describe("Minimax", func() {
+
+})
