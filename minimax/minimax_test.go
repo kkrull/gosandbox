c148fdf5f69dcf99abad79ec2e864dadec1789a5
Kyle Krull <kkrull@8thlight.com>
2018-03-29 09:42:55 -0500

    Rename

--- minimax/minimax_test.go
+++ minimax/minimax_test.go
@@ -10,31 +10,33 @@ import (
 var _ = Describe("Minimax", func() {
 	Context("given a game that the maximizing player has won", func() {
 		It("scores that game as +1", func() {
-			game := VictoryGame{Winner: "Max"}
+			game := CompletedGame{Winner: "Max"}
 			Expect(Minimax(game, "Max")).To(BeEquivalentTo(Result{Score: 1}))
 		})
 	})
 
 	Context("given a game that the minimizing player has won", func() {
 		It("scores that game as -1", func() {
-			game := VictoryGame{Winner: "Min"}
+			game := CompletedGame{Winner: "Min"}
 			Expect(Minimax(game, "Min")).To(BeEquivalentTo(Result{Score: -1}))
 		})
 	})
 
 	Context("given a game ending in a draw", func() {
 		It("scores that game as -1", func() {
-			game := VictoryGame{}
+			game := CompletedGame{}
 			Expect(Minimax(game, "Max")).To(BeEquivalentTo(Result{Score: 0}))
 		})
 	})
+	
+
 })
 
-type VictoryGame struct {
+type CompletedGame struct {
 	Winner string
 }
 
-func (game VictoryGame) FindWinner() string {
+func (game CompletedGame) FindWinner() string {
 	return game.Winner
 }
 
