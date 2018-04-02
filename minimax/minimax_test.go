ed52ed9d289be7e121dba85350c555373d0f6952
Kyle Krull <kkrull@8thlight.com>
2018-04-02 08:28:42 -0500

    Up and running

--- minimax/minimax_test.go
+++ minimax/minimax_test.go
@@ -2,11 +2,14 @@ package minimax_test
 
 import (
 	. "github.com/onsi/ginkgo"
-	//. "github.com/onsi/gomega"
+	. "github.com/onsi/gomega"
 
-	//. "github.com/kkrull/gosandbox/minimax"
+	. "github.com/kkrull/gosandbox/minimax"
 )
 
 var _ = Describe("Minimax", func() {
-
+	It("imports", func() {
+		Expect(42).To(Equal(42))
+		Minimax()
+	})
 })
