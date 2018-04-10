81a78c6733bc7a24367202da291c5a316ca6d3ec
Kyle Krull <kkrull@8thlight.com>
2018-04-10 09:49:15 -0500

    Trying to force recursion

--- minimax/minimax_test.go
+++ minimax/minimax_test.go
@@ -37,11 +37,20 @@ var _ = Describe("Negamax", func() {
 		game := FakeGame{isOver: true, winner: min}
 		Expect(Negamax(game, min)).To(Equal(1))
 	})
+
+	XIt("evaluates the maximum score the maximizing player can get, if the minimizing player tries to win", func() {
+		game := FakeGame{isOver: false, next: []Game{
+			FakeGame{isOver: true},
+			FakeGame{isOver: true, winner: max},
+		}}
+		Expect(Negamax(game, max)).To(Equal(0))
+	})
 })
 
 type FakeGame struct {
 	isOver bool
 	winner Player
+	next []Game
 }
 
 func (game FakeGame) FindWinner() Player {
