a7a0372222ef5e66716bee94e66d6ffc1d30673b
Kyle Krull <kkrull@8thlight.com>
2018-04-10 09:40:34 -0500

    Score for win by max

--- minimax/minimax.go
+++ minimax/minimax.go
@@ -1,11 +1,16 @@
 package minimax
 
 func Minimax(game Game, player Player) int {
+	if game.FindWinner() == game.MaximizingPlayer() {
+		return 1
+	}
+
 	return 0
 }
 
 type Game interface {
-
+	FindWinner() Player
+	MaximizingPlayer() Player
 }
 
 type Player struct {
--- minimax/minimax_test.go
+++ minimax/minimax_test.go
@@ -16,8 +16,22 @@ var _ = Describe("Minimax", func() {
 		game := FakeGame{isOver: true}
 		Expect(Minimax(game, max)).To(Equal(0))
 	})
+
+	It("scores a game won by the maximizing player as +1", func() {
+		game := FakeGame{isOver: true, winner: max}
+		Expect(Minimax(game, max)).To(Equal(1))
+	})
 })
 
 type FakeGame struct {
 	isOver bool
+	winner Player
+}
+
+func (game FakeGame) FindWinner() Player {
+	return game.winner
+}
+
+func (game FakeGame) MaximizingPlayer() Player {
+	return max
 }
