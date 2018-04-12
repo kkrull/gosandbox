63ac531c8b6e02b89270794bb6bb1e975b94524d
Kyle Krull <kkrull@8thlight.com>
2018-04-12 09:15:56 -0500

    Scoring a win for min

--- minimax/minimax_test.go
+++ minimax/minimax_test.go
@@ -9,6 +9,7 @@ import (
 
 var (
 	max = Player{Name: "max"}
+	min = Player{Name: "min"}
 )
 
 var _ = Describe("Minimax", func() {
@@ -20,6 +21,11 @@ var _ = Describe("Minimax", func() {
 	It("scores a game won by the maximizing player as +1", func() {
 		game := FakeGame{over: true, winner: max}
 		Expect(Minimax(game, max)).To(Equal(1))
+	})
+
+	It("scores a game won by the minimizing player as -1", func() {
+		game := FakeGame{over: true, winner: min}
+		Expect(Minimax(game, max)).To(Equal(-1))
 	})
 })
 
@@ -38,4 +44,8 @@ func (game FakeGame) IsOver() bool {
 
 func (game FakeGame) MaximizingPlayer() Player {
 	return max
+}
+
+func (game FakeGame) MinimizingPlayer() Player {
+	return min
 }
