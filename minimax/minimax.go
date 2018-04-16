c4eccda4268c40c1e65bc081d7b4cabeaccd34ec
Kyle Krull <kkrull@8thlight.com>
2018-04-16 09:31:54 -0500

    Parameters

--- minimax/minimax.go
+++ minimax/minimax.go
@@ -1,5 +1,13 @@
 package minimax
 
-func Minimax() int {
+func Minimax(game Game, player Player) int {
 	return 0
+}
+
+type Game interface {
+
+}
+
+type Player struct {
+	Name string
 }
--- minimax/minimax_test.go
+++ minimax/minimax_test.go
@@ -7,8 +7,17 @@ import (
 	. "github.com/kkrull/gosandbox/minimax"
 )
 
+var (
+	max = Player{Name: "max"}
+)
+
 var _ = Describe("Minimax", func() {
 	It("scores a game ending in a draw as 0", func() {
-		Expect(Minimax()).To(Equal(0))
+		game := FakeGame{}
+		Expect(Minimax(game, max)).To(Equal(0))
 	})
 })
+
+type FakeGame struct {
+
+}
