6fdf2a6bbf741919530d1065ad01d980509aa6e7
Kyle Krull <kkrull@8thlight.com>
2018-04-17 08:01:28 -0500

    Draw game

--- minimax/minimax.go
+++ minimax/minimax.go
@@ -1,2 +1,17 @@
 package minimax
 
+func Minimax(game Game, player Player) int {
+	if game.IsOver() {
+		return 0
+	}
+
+	return -999
+}
+
+type Game interface {
+	IsOver() bool
+}
+
+type Player struct {
+	Name string
+}
--- minimax/minimax_test.go
+++ minimax/minimax_test.go
@@ -2,11 +2,24 @@ package minimax_test
 
 import (
 	. "github.com/onsi/ginkgo"
-	//. "github.com/onsi/gomega"
+	. "github.com/onsi/gomega"
 
-	//. "github.com/kkrull/gosandbox/minimax"
+	. "github.com/kkrull/gosandbox/minimax"
 )
 
 var _ = Describe("Minimax", func() {
-
+	It("scores a game ending in a draw as 0", func() {
+		game := FakeGame{isOver: true}
+		max := Player{Name: "Max"}
+		Expect(Minimax(game, max)).To(Equal(0))
+	})
 })
+
+type FakeGame struct {
+	isOver bool
+}
+
+func (game FakeGame) IsOver() bool {
+	return game.isOver
+}
+
