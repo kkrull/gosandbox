cadcea5bc6c3e92f9abd16fecb6fcb1a58c6cc42
Kyle Krull <kkrull@8thlight.com>
2018-04-19 09:18:05 -0500

    Draw score

--- minimax/minimax.go
+++ minimax/minimax.go
@@ -1,11 +1,15 @@
 package minimax
 
 func Minimax(game Game, player Player) int {
+	if game.IsOver() {
+		return 0
+	}
+
 	return 999
 }
 
 type Game interface {
-
+	IsOver() bool
 }
 
 type Player struct {
--- minimax/minimax_test.go
+++ minimax/minimax_test.go
@@ -9,12 +9,16 @@ import (
 
 var _ = Describe("Minimax", func() {
 	It("scores a game ending in a draw as 0", func() {
-		game := FakeGame{}
+		game := FakeGame{isOver: true}
 		max := Player{Name: "Max"}
 		Expect(Minimax(game, max)).To(Equal(0))
 	})
 })
 
 type FakeGame struct {
+	isOver bool
+}
 
+func (game FakeGame) IsOver() bool {
+	return game.isOver
 }
