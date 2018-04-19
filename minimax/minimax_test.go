6c73d755fb2431161b0c204b68eeab2d8ffef8c9
Kyle Krull <kkrull@8thlight.com>
2018-04-19 09:19:39 -0500

    win for max

--- minimax/minimax_test.go
+++ minimax/minimax_test.go
@@ -7,18 +7,35 @@ import (
 	. "github.com/kkrull/gosandbox/minimax"
 )
 
+var (
+	max = Player{Name: "Max"}
+)
+
 var _ = Describe("Minimax", func() {
 	It("scores a game ending in a draw as 0", func() {
 		game := FakeGame{isOver: true}
-		max := Player{Name: "Max"}
 		Expect(Minimax(game, max)).To(Equal(0))
+	})
+
+	It("scores a game won by the maximizing player as +1", func() {
+		game := FakeGame{isOver: true, winner: max}
+		Expect(Minimax(game, max)).To(Equal(1))
 	})
 })
 
 type FakeGame struct {
 	isOver bool
+	winner Player
+}
+
+func (game FakeGame) FindWinner() Player {
+	return game.winner
 }
 
 func (game FakeGame) IsOver() bool {
 	return game.isOver
+}
+
+func (game FakeGame) MaximizingPlayer() Player {
+	return max
 }
