4ddfe4da310f20a07427e48cc0f29c31742231fc
Kyle Krull <kkrull@8thlight.com>
2018-04-20 14:04:04 -0500

    The final product

--- minimax/minimax.go
+++ minimax/minimax.go
@@ -1,2 +1,42 @@
 package minimax
 
+func Minimax(game Game, player Player) int {
+	if player == game.MaximizingPlayer() {
+		return Negamax(game, 1)
+	} else {
+		return -Negamax(game, -1)
+	}
+}
+
+func Negamax(game Game, polarity int) int {
+	if game.FindWinner() == game.MaximizingPlayer() {
+		return 1 * polarity
+	} else if game.FindWinner() == game.MinimizingPlayer() {
+		return -1 * polarity
+	} else if game.IsOver() {
+		return 0
+	}
+
+	bestScore := -100
+	for _, nextGame := range game.NextPossibleGames() {
+		scoreOfThatGameState := -Negamax(nextGame, -1 * polarity)
+		if scoreOfThatGameState > bestScore {
+			bestScore = scoreOfThatGameState
+		}
+	}
+
+	return bestScore
+}
+
+type Game interface {
+	FindWinner() Player
+	IsOver() bool
+	MaximizingPlayer() Player
+	MinimizingPlayer() Player
+	NextPossibleGames() []Game
+}
+
+type Player struct {
+	Name string
+}
+
--- minimax/minimax_test.go
+++ minimax/minimax_test.go
@@ -2,11 +2,71 @@ package minimax_test
 
 import (
 	. "github.com/onsi/ginkgo"
-	//. "github.com/onsi/gomega"
+	. "github.com/onsi/gomega"
 
-	//. "github.com/kkrull/gosandbox/minimax"
+	. "github.com/kkrull/gosandbox/minimax"
+)
+
+var (
+	max = Player{Name: "max"}
+	min = Player{Name: "min"}
 )
 
 var _ = Describe("Minimax", func() {
+	It("scores a game ending in a draw as 0", func() {
+		game := &FakeGame{isOver: true}
+		Expect(Minimax(game, max)).To(Equal(0))
+	})
 
+	It("scores a game won by the maximizing player as +1", func() {
+		game := &FakeGame{isOver: true, winner: max}
+		Expect(Minimax(game, max)).To(Equal(1))
+	})
+
+	It("scores a game won by the minimizing player as -1", func() {
+		game := &FakeGame{isOver: true, winner: min}
+		Expect(Minimax(game, max)).To(Equal(-1))
+	})
+
+	It("returns the maximum score that the maximizing player can achieve in a non-terminal game", func() {
+		game := &FakeGame{nextPossibleGames: []Game{
+			&FakeGame{isOver: true},
+			&FakeGame{isOver: true, winner: max},
+		}}
+		Expect(Minimax(game, max)).To(Equal(1))
+	})
+
+	It("returns the minimum score that the minimizing player can achieve in a non-terminal game", func() {
+		game := &FakeGame{nextPossibleGames: []Game{
+			&FakeGame{isOver: true},
+			&FakeGame{isOver: true, winner: min},
+		}}
+		Expect(Minimax(game, min)).To(Equal(-1))
+	})
 })
+
+type FakeGame struct {
+	isOver bool
+	winner Player
+	nextPossibleGames []Game
+}
+
+func (game *FakeGame) FindWinner() Player {
+	return game.winner
+}
+
+func (game *FakeGame) MaximizingPlayer() Player {
+	return max
+}
+
+func (game *FakeGame) MinimizingPlayer() Player {
+	return min
+}
+
+func (game *FakeGame) NextPossibleGames() []Game {
+	return game.nextPossibleGames
+}
+
+func (game *FakeGame) IsOver() bool {
+	return game.isOver
+}
