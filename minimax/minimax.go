cf918db96e9d57f780ba9bbb9dfe28bc595a0649
Kyle Krull <kkrull@8thlight.com>
2018-04-26 10:04:09 -0500

    Refactored to negamax

--- minimax/minimax.go
+++ minimax/minimax.go
@@ -6,37 +6,32 @@ type Scorer struct {
 }
 
 func (scorer *Scorer) Score(game Game, player Player) int {
+	if player == scorer.MaximizingPlayer {
+		return scorer.Negamax(game, 1)
+	} else {
+		return -scorer.Negamax(game, -1)
+	}
+}
+
+func (scorer *Scorer) Negamax(game Game, polarity int) int {
 	if game.FindWinner() == scorer.MaximizingPlayer {
-		return 1
+		return 1 * polarity
 	} else if game.FindWinner() == scorer.MinimizingPlayer {
-		return -1
+		return -1 * polarity
 	} else if game.IsOver() {
 		return 0
 	}
 
-	if player == scorer.MaximizingPlayer {
-		maxScore := -100
-		for _, move := range game.AvailableMoves() {
-			nextGame := game.Move(move)
-			nextScore := scorer.Score(nextGame, scorer.MinimizingPlayer)
-			if nextScore > maxScore {
-				maxScore = nextScore
-			}
+	maxScore := -100
+	for _, move := range game.AvailableMoves() {
+		nextGame := game.Move(move)
+		nextScore := -scorer.Negamax(nextGame, -1 * polarity)
+		if nextScore > maxScore {
+			maxScore = nextScore
 		}
-
-		return maxScore
-	} else {
-		minScore := 100
-		for _, move := range game.AvailableMoves() {
-			nextGame := game.Move(move)
-			nextScore := scorer.Score(nextGame, scorer.MaximizingPlayer)
-			if nextScore < minScore {
-				minScore = nextScore
-			}
-		}
-
-		return minScore
 	}
+
+	return maxScore
 }
 
 type Game interface {
