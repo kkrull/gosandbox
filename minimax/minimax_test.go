6eea491308d2b5ff5162e463053be15e9bbc8fe0
Kyle Krull <kkrull@8thlight.com>
2018-04-26 09:32:35 -0500

    More naming

--- minimax/minimax_test.go
+++ minimax/minimax_test.go
@@ -19,48 +19,48 @@ var (
 var _ = Describe("Minimax", func() {
 	Describe("#Score", func() {
 		It("scores a game ending in a draw as 0", func() {
-			game := &PredeterminedGame{isOver: true}
+			game := &GameWithKnownStates{isOver: true}
 			Expect(scorer.Score(game, max)).To(Equal(0))
 		})
 
 		It("scores a game won by the maximizing player as +1", func() {
-			game := &PredeterminedGame{isOver: true, winner: max}
+			game := &GameWithKnownStates{isOver: true, winner: max}
 			Expect(scorer.Score(game, max)).To(Equal(1))
 		})
 
 		It("scores a game won by the minimizing player as -1", func() {
-			game := &PredeterminedGame{isOver: true, winner: min}
+			game := &GameWithKnownStates{isOver: true, winner: min}
 			Expect(scorer.Score(game, max)).To(Equal(-1))
 		})
 
 		It("maximizes the score for the maximizing player, among all possible outcomes", func() {
-			game := &PredeterminedGame{}
-			game.MoveLeadsTo(Move{Id: "Max Chooses....Poorly"}, &PredeterminedGame{isOver: true})
-			game.MoveLeadsTo(Move{Id: "Max Chooses....Wisely"}, &PredeterminedGame{isOver: true, winner: max})
+			game := &GameWithKnownStates{}
+			game.AddStateTransition(Move{Id: "Max Chooses....Poorly"}, &GameWithKnownStates{isOver: true})
+			game.AddStateTransition(Move{Id: "Max Chooses....Wisely"}, &GameWithKnownStates{isOver: true, winner: max})
 			Expect(scorer.Score(game, max)).To(Equal(1))
 		})
 
 		It("minimizes the score for the minimizing player, among all possible outcomes", func() {
-			game := &PredeterminedGame{}
-			game.MoveLeadsTo(Move{Id: "Min Chooses....Poorly"}, &PredeterminedGame{isOver: true})
-			game.MoveLeadsTo(Move{Id: "Min Chooses....Wisely"}, &PredeterminedGame{isOver: true, winner: min})
+			game := &GameWithKnownStates{}
+			game.AddStateTransition(Move{Id: "Min Chooses....Poorly"}, &GameWithKnownStates{isOver: true})
+			game.AddStateTransition(Move{Id: "Min Chooses....Wisely"}, &GameWithKnownStates{isOver: true, winner: min})
 			Expect(scorer.Score(game, min)).To(Equal(-1))
 		})
 	})
 })
 
-type PredeterminedGame struct {
+type GameWithKnownStates struct {
 	isOver    bool
 	winner    Player
 	nextMoves []Move
 	nextGames []Game
 }
 
-func (game *PredeterminedGame) AvailableMoves() []Move {
+func (game *GameWithKnownStates) AvailableMoves() []Move {
 	return game.nextMoves
 }
 
-func (game *PredeterminedGame) Move(selectedMove Move) Game {
+func (game *GameWithKnownStates) Move(selectedMove Move) Game {
 	for i, move := range game.nextMoves {
 		if move == selectedMove	{
 			return game.nextGames[i]
@@ -70,15 +70,15 @@ func (game *PredeterminedGame) Move(selectedMove Move) Game {
 	panic("Unrecognized move")
 }
 
-func (game *PredeterminedGame) FindWinner() Player {
+func (game *GameWithKnownStates) FindWinner() Player {
 	return game.winner
 }
 
-func (game *PredeterminedGame) IsOver() bool {
+func (game *GameWithKnownStates) IsOver() bool {
 	return game.isOver
 }
 
-func (game *PredeterminedGame) MoveLeadsTo(move Move, nextGame *PredeterminedGame) {
+func (game *GameWithKnownStates) AddStateTransition(move Move, nextGame *GameWithKnownStates) {
 	game.nextMoves = append(game.nextMoves, move)
 	game.nextGames = append(game.nextGames, nextGame)
 }
