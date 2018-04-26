94c2c262b84fb1eb35e66d5d05ed3f7479f8eaee
Kyle Krull <kkrull@8thlight.com>
2018-04-26 09:51:54 -0500

    Refactor

--- minimax/minimax_test.go
+++ minimax/minimax_test.go
@@ -7,13 +7,13 @@ import (
 )
 
 var (
-	max = Player{Name: "Max"}
+	max    = Player{Name: "Max"}
+	scorer = minimax.Scorer{}
 )
 
 var _ = Describe("Scorer", func() {
 	Describe("#Score", func() {
 		It("scores a game ending in a draw as 0", func() {
-			scorer := minimax.Scorer{}
 			game := &GameWithKnownStates{isOver: true}
 			Expect(scorer.Score(game, max)).To(Equal(0))
 		})
