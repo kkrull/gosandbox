f31f367dc24a0aef9c0899d005de81095928dfc4
Kyle Krull <kkrull@8thlight.com>
2018-06-01 14:37:08 -0500

    New starting point for minimax, where the types are declared

--- minimax/minimax.go
+++ minimax/minimax.go
@@ -1,2 +1,14 @@
 package minimax
 
+type Scorer struct {
+}
+
+func (scorer *Scorer) Score(game GameState, player Player) int {
+	panic("Not implemented")
+}
+
+type GameState interface {
+}
+
+type Move string
+type Player string
--- minimax/minimax_test.go
+++ minimax/minimax_test.go
@@ -1,11 +1,28 @@
 package minimax_test
 
 import (
+	"github.com/kkrull/gosandbox/minimax"
 	. "github.com/onsi/ginkgo"
-	//. "github.com/onsi/gomega"
+	. "github.com/onsi/gomega"
 )
 
 var _ = Describe("Scorer", func() {
+	var (
+		scorer *minimax.Scorer
+		game   *GameWithKnownStates
+	)
+
 	Describe("#Score", func() {
+		BeforeEach(func() {
+			scorer = &minimax.Scorer{}
+		})
+
+		It("scores a game ending in a draw as 0", func() {
+			game = &GameWithKnownStates{}
+			Expect("Universal Answer").To(Equal(42))
+		})
 	})
 })
+
+type GameWithKnownStates struct {
+}
