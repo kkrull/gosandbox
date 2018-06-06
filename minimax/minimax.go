fecab5c5e4296ceff99e8d2ec4c4a1f6806b4e23
Kyle Krull <kkrull@8thlight.com>
2018-06-06 08:12:49 -0500

    11:10
    Theme of Bartz
    Advent: One-Winged Angel
    Part of Cosmo Canyon

--- minimax/minimax.go
+++ minimax/minimax.go
@@ -1,13 +1,44 @@
 package minimax
 
 type Scorer struct {
+	Maximizer Player
+	Minimizer Player
 }
 
 func (scorer *Scorer) Score(game GameState, player Player) int {
-	panic("Not implemented")
+	if player == scorer.Maximizer {
+		return scorer.Negamax(game, 1)
+	} else {
+		return -scorer.Negamax(game, -1)
+	}
+}
+
+func (scorer *Scorer) Negamax(game GameState, polarity int) int {
+	if game.FindWinner() == scorer.Maximizer {
+		return 1 * polarity
+	} else if game.FindWinner() == scorer.Minimizer {
+		return -1 * polarity
+	} else if game.IsOver() {
+		return 0
+	}
+
+	maxScore := -100
+	for _, nextMove := range game.AvailableMoves() {
+		nextGame := game.Move(nextMove)
+		nextScore := -scorer.Negamax(nextGame, -1*polarity)
+		if nextScore > maxScore {
+			maxScore = nextScore
+		}
+	}
+
+	return maxScore
 }
 
 type GameState interface {
+	AvailableMoves() []Move
+	IsOver() bool
+	FindWinner() Player
+	Move(move Move) GameState
 }
 
 type Move string
