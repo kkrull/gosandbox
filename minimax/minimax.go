81e0d7dd94198fb3d0dfbd6ad4cc59aa90d148c8
Kyle Krull <kkrull@8thlight.com>
2018-06-21 08:03:17 -0500

    Second verse of Cosmo Canyon, this time with reverseThePolarity
    This one's for you Eric

--- minimax/minimax.go
+++ minimax/minimax.go
@@ -1,13 +1,48 @@
 package minimax
 
 type Scorer struct {
+	Maximizer Player
+	Minimizer Player
 }
 
 func (scorer *Scorer) Score(game GameState, player Player) int {
-	panic("Not implemented")
+	if player == scorer.Maximizer {
+		return scorer.negamax(game, 1)
+	} else {
+		return -scorer.negamax(game, -1)
+	}
+}
+
+func (scorer *Scorer) negamax(game GameState, polarity int) int {
+	if game.FindWinner() == scorer.Maximizer {
+		return 1 * polarity
+	} else if game.FindWinner() == scorer.Minimizer {
+		return -1 * polarity
+	} else if game.IsOver() {
+		return 0
+	}
+
+	maxScore := -100
+	for _, nextMove := range game.AvailableMoves() {
+		nextGame := game.Move(nextMove)
+		nextScore := reverseThePolarity(scorer.negamax(nextGame, reverseThePolarity(polarity)))
+		if nextScore > maxScore {
+			maxScore = nextScore
+		}
+	}
+
+	return maxScore
+}
+
+func reverseThePolarity(polarity int) int {
+	return -1 * polarity
 }
 
 type GameState interface {
+	AvailableMoves() []Move
+	IsOver() bool
+	FindWinner() Player
+	Move(move Move) GameState
 }
 
 type Move string
