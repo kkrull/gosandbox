80f3e2f848e2422d928cfc36f2379e27057eb6bc
Kyle Krull <kkrull@8thlight.com>
2018-06-27 08:09:09 -0500

    Intermediate state with the extra test/step

--- minimax/minimax.go
+++ minimax/minimax.go
@@ -1,13 +1,29 @@
 package minimax
 
 type Scorer struct {
+	Maximizer Player
+	Minimizer Player
 }
 
 func (scorer *Scorer) Score(game GameState, player Player) int {
-	panic("Not implemented")
+	if game.FindWinner() == scorer.Maximizer {
+		return 1
+	} else if game.FindWinner() == scorer.Minimizer {
+		return -1
+	} else if game.IsOver() {
+		return 0
+	}
+
+	nextMove := game.AvailableMoves()[0]
+	nextGame := game.Move(nextMove)
+	return scorer.Score(nextGame, Player("Who?"))
 }
 
 type GameState interface {
+	AvailableMoves() []Move
+	FindWinner() Player
+	IsOver() bool
+	Move(move Move) GameState
 }
 
 type Move string
--- minimax/minimax_test.go
+++ minimax/minimax_test.go
@@ -10,16 +10,74 @@ var _ = Describe("Scorer", func() {
 	var (
 		game   *GameWithKnownStates
 		max    = minimax.Player("Max")
-		scorer = &minimax.Scorer{}
+		min    = minimax.Player("Min")
+		scorer = &minimax.Scorer{
+			Maximizer: max,
+			Minimizer: min,
+		}
 	)
 
 	Describe("#Score", func() {
 		It("scores a game ending in a draw as 0", func() {
-			game = &GameWithKnownStates{}
+			game = &GameWithKnownStates{isOver: true}
 			Expect(scorer.Score(game, max)).To(Equal(0))
+		})
+
+		It("scores a game won by the maximizer as +1", func() {
+			game = &GameWithKnownStates{isOver: true, winner: max}
+			Expect(scorer.Score(game, max)).To(Equal(1))
+		})
+
+		It("scores a game won by the minimizer as -1", func() {
+			game = &GameWithKnownStates{isOver: true, winner: min}
+			Expect(scorer.Score(game, max)).To(Equal(-1))
+		})
+
+		It("the player picks a move in an unfinished game", func() {
+			game = &GameWithKnownStates{}
+			game.AddKnownState(minimax.Move("MaxWins"), &GameWithKnownStates{isOver: true, winner: max})
+			Expect(scorer.Score(game, max)).To(Equal(1))
+		})
+
+		It("the maximizer picks the move with the highest score, in an unfinished game", func() {
+			game = &GameWithKnownStates{}
+			game.AddKnownState(minimax.Move("Draw"), &GameWithKnownStates{isOver: true})
+			game.AddKnownState(minimax.Move("MaxWins"), &GameWithKnownStates{isOver: true, winner: max})
+			Expect(scorer.Score(game, max)).To(Equal(1))
 		})
 	})
 })
 
 type GameWithKnownStates struct {
+	isOver    bool
+	winner    minimax.Player
+	nextMoves []minimax.Move
+	nextGames []*GameWithKnownStates
+}
+
+func (game *GameWithKnownStates) AvailableMoves() []minimax.Move {
+	return game.nextMoves
+}
+
+func (game *GameWithKnownStates) AddKnownState(nextMove minimax.Move, nextGame *GameWithKnownStates) {
+	game.nextMoves = append(game.nextMoves, nextMove)
+	game.nextGames = append(game.nextGames, nextGame)
+}
+
+func (game *GameWithKnownStates) FindWinner() minimax.Player {
+	return game.winner
+}
+
+func (game *GameWithKnownStates) IsOver() bool {
+	return game.isOver
+}
+
+func (game *GameWithKnownStates) Move(move minimax.Move) minimax.GameState {
+	for i, nextMove := range game.nextMoves {
+		if nextMove == move {
+			return game.nextGames[i]
+		}
+	}
+
+	panic(move)
 }
