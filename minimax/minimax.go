7e6ead8cbc3265f8b831e016e21f2bd74d282bbf
Kyle Krull <kdkrull@gmail.com>
2024-06-25 09:50:19 -0500

    Fill in missing tests and code

--- minimax/minimax.go
+++ minimax/minimax.go
@@ -1,14 +1,53 @@
 package minimax
 
-type Scorer struct{}
-
-func (scorer *Scorer) Score(game GameState, player Player) int {
-	panic("Not implemented")
+type Scorer struct {
+	Maximizer Player
+	Minimizer Player
 }
 
-type GameState interface{}
+func (scorer *Scorer) Score(game GameState, player Player) int {
+	if player == scorer.Maximizer {
+		return scorer.negamax(game, 1)
+	} else {
+		return reversePerspective(scorer.negamax(game, -1))
+	}
+}
 
-type (
-	Move   string
-	Player string
-)
+func (scorer *Scorer) negamax(game GameState, polarity int) int {
+	if game.FindWinner() == scorer.Maximizer {
+		return 1 * polarity
+	} else if game.FindWinner() == scorer.Minimizer {
+		return reverseThe(polarity)
+	} else if game.IsOver() {
+		return 0
+	}
+
+	maxScore := -10
+	for _, nextMove := range game.AvailableMoves() {
+		nextGame := game.Move(nextMove)
+		nextScore := reversePerspective(scorer.negamax(nextGame, reverseThe(polarity)))
+		if nextScore > maxScore {
+			maxScore = nextScore
+		}
+	}
+
+	return maxScore
+}
+
+func reversePerspective(score int) int {
+	return -1 * score
+}
+
+func reverseThe(polarity int) int {
+	return -1 * polarity
+}
+
+type GameState interface {
+	AvailableMoves() []Move
+	FindWinner() Player
+	IsOver() bool
+	Move(move Move) GameState
+}
+
+type Move string
+type Player string
