8b87bf68ba06f11af6ffb82423c08acedbff55bb
Kyle Krull <kdkrull@gmail.com>
2024-06-28 00:35:47 -0500

    Read a flag

--- cobra-hello/rootCmd/root.go
+++ cobra-hello/rootCmd/root.go
@@ -7,21 +7,26 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var rootCmd = &cobra.Command{
-	Long: `cobrahello demonstrates how to use Cobra to parse command line arguments`,
-	Run: func(cmd *cobra.Command, positionalArgs []string) {
-		fmt.Println("rootCmd positional args:")
-		for i, arg := range positionalArgs {
-			fmt.Printf("- %d: %s\n", i, arg)
-		}
-	},
-	Short:   "Demonstrates how to use Cobra",
-	Use:     "cobrahello",
-	Version: "0.0.1",
-}
+var (
+	homeDirFlag *string
+	rootCmd     = &cobra.Command{
+		Long: `cobrahello demonstrates how to use Cobra to parse command line arguments`,
+		Run: func(cmd *cobra.Command, positionalArgs []string) {
+			fmt.Printf("homeDir: %s\n", *homeDirFlag)
+
+			fmt.Println("rootCmd positional args:")
+			for i, arg := range positionalArgs {
+				fmt.Printf("- %d: %s\n", i, arg)
+			}
+		},
+		Short:   "Demonstrates how to use Cobra",
+		Use:     "cobrahello",
+		Version: "0.0.1",
+	}
+)
 
 func init() {
-	rootCmd.Flags().String("home-dir", "<default-value>", "some description of what this means")
+	homeDirFlag = rootCmd.Flags().String("home-dir", "<default-value>", "some description of what this means")
 }
 
 func Execute() {
