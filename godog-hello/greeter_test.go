69736d3e8661f92d16b43179c46d0a540aaca155
Kyle Krull <kdkrull@gmail.com>
2024-07-02 17:47:59 -0500

    Integrate godog with Testing

--- godog-hello/greeter_test.go
+++ godog-hello/greeter_test.go
@@ -1,7 +1,25 @@
 package main_test
 
-import "github.com/cucumber/godog"
+import (
+	"testing"
+
+	"github.com/cucumber/godog"
+)
 
 func InitializeScenario(ctx *godog.ScenarioContext) {
+}
 
+func TestFeatures(t *testing.T) {
+	suite := godog.TestSuite{
+		Options: &godog.Options{
+			Format: "pretty",
+			Paths: []string{"features"},
+			TestingT: t,
+		},
+		ScenarioInitializer: InitializeScenario,
+	}
+
+	if suite.Run() != 0 {
+		t.Fatal("non-zero status returned from feature tests")
+	}
 }
