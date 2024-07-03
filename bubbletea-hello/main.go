e1c5384c28b6489446cc3237e70da08e07ef98da
Kyle Krull <kdkrull@gmail.com>
2024-07-02 23:23:09 -0500

    Add bubbletea

--- bubbletea-hello/main.go
+++ bubbletea-hello/main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	tea "github.com/charmbracelet/bubbletea"
+)
 
 func main() {
 	fmt.Printf("Hello World!\n")
+}
+
+type model struct {
+	Message string
+}
+
+func initialModel() *model {
+	return &model{Message: "Hello World!"}
+}
+
+func (m model) Init() tea.Cmd {
+	return nil
 }
