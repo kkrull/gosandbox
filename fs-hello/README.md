f2b8a521f7447041d1ce58c237624551a1a2a643
Kyle Krull <kdkrull@gmail.com>
2024-07-03 17:36:12 -0500

    Inspect file information

--- fs-hello/README.md
+++ fs-hello/README.md
@@ -1,3 +1,7 @@
 # Hello `fs`
 
 A place to interact with the filesystem, using the built-in `fs` package.
+
+## Documentation
+
+- `fs` package: <https://pkg.go.dev/io/fs>
--- fs-hello/main.go
+++ fs-hello/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/fs"
+	"os"
+)
 
 func main() {
-	fmt.Printf("Hello World!")
+	currentDirectory := "."
+	fileSystem := os.DirFS(currentDirectory)
+	fmt.Printf("Listing files in: %s\n", fileSystem)
+	fs.WalkDir(fileSystem, ".", func(filename string, entry fs.DirEntry, err error) error {
+		info, _ := entry.Info()
+		fileType := entry.Type()
+		fmt.Printf(
+			"filename=%s, mode=%s, name=%s, type=%s\n",
+			filename,
+			info.Mode(),
+			info.Name(),
+			fileType.String(),
+		)
+
+		return nil
+	})
 }
